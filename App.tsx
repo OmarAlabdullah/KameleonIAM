50414f89031bef89d5dc844f075b382ab4c52d6f
Omar Alabdullah <omar@xel.nl>
2022-12-20 13:26:41 +0100

    #4158  add Auth0 provider to App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import {Auth0Provider} from 'react-native-auth0';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {LoginScreen} from './Screens/Login';
+import config from './auth0-config';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={LoginScreen}
-          options={{title: 'Login'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Auth0Provider domain={config.domain} clientId={config.clientId}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{title: 'Login'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Auth0Provider>
   );
 };
 
